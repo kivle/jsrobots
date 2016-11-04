b2c86a2cae340c03f5120753ffff315ebc5b2447
Helge Elvik <Helge Elvik>
2016-11-05 00:12:22 +0100

    Initial setup

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+// Main entrypoint to the application
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render((
+    <div>Hello world</div>
+    ), 
+    document.getElementById('root')
+);
---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/phaser/index.d.ts" />
+/// <reference path="modules/react-dom/index.d.ts" />
+/// <reference path="modules/react/index.d.ts" />
+/// <reference path="modules/webpack/index.d.ts" />
